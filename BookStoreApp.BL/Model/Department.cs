022cc35cd339d8298a73a1dec701a140a1de494a
Azizkhon Isomadinov <isomadinovv@gmail.com>
2022-03-17 12:50:05 +0700

    Изменение в классе Department

--- BookStoreApp.BL/Model/Department.cs
+++ BookStoreApp.BL/Model/Department.cs
@@ -18,33 +18,32 @@ namespace BookStoreApp.BL.Model
         /// Создать новый отдел.
         /// </summary>
         /// <param name="nameDepartment"> Название отдела. </param>
-        public Department(string nameDepartment, List<Book> books)
+        public Department(string nameDepartment)
         {
             #region Проверка условий
             if (string.IsNullOrWhiteSpace(nameDepartment))
             {
                 throw new ArgumentNullException("Название отдела не может быть пустым.", nameof(nameDepartment));
             }
-            if (books == null)
-            {
-                throw new ArgumentNullException("Информация введена не корректно.", nameof(books));
-            }
             #endregion
-
             NameDepartment = nameDepartment;
-            Books = books;
         }
-
 
         public Department()
         {
             NameDepartment = "Название отдела";
-            Books = new List<Book>();
         }
 
         public override string ToString()
         {
             return $"Название отдела:{NameDepartment}.\nКниги в отделе:{Books}.";
+        }
+
+        public void CreateNewDepartment(string nameDepartment, string nameBook, double priceBook, double discountBook)
+        {
+            Department department = new Department(nameDepartment);
+            Book newBook = new Book(nameBook, priceBook, discountBook);
+            Books.Add(newBook);
         }
     }
 }
